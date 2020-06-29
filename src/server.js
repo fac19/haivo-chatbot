5a09dd7f3834b688e471008ffb93e076e5aa8396
Ivo Evans <ievans.storrie@gmail.com>
2020-06-29 14:07:46 +0100

    do workshop
    Co-authored-by: HannahGooding <hannah.gooding@yahoo.com>

--- src/server.js
+++ src/server.js
@@ -16,4 +16,12 @@ app.get("/", (req, res) => {
   }
 });
 
+app.post("/appointments", (req, res) => {
+  const date_time = req.body.queryResult.parameters["date-time"].date_time
+  const fulfillmentMessages = req.body.queryResult.fulfillmentMessages
+  
+  db.get("appointments").push({ date_time: date_time}).write()
+  res.status(200).json({fulfillmentMessages})
+})
+
 module.exports = app;
