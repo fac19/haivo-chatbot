c12d54b7a7a053ed8ec775af4d4a55d09151da86
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-29 16:22:31 +0100

    Update mood route response, add README
    Co-authored-by: Ivo-Evans <ievans.storrie@gmail.com>

--- src/server.js
+++ src/server.js
@@ -16,12 +16,65 @@ app.get("/", (req, res) => {
   }
 });
 
-app.post("/appointments", (req, res) => {
-  const date_time = req.body.queryResult.parameters["date-time"].date_time
+app.post("/mood", (req, res) => {
+  const mood = req.body.queryResult.parameters.mood
   const fulfillmentMessages = req.body.queryResult.fulfillmentMessages
+  console.log({fulfillmentMessages})
+  let text = "";
+
+  switch(mood) {
+  case "tired":
+    text = "When you are feeling tired, you should wear pyjamas and go to bed.";
+    break;
+  case "happy":
+    text = "If you're happy, you should wear a crown!";
+    break;
+  case "flamboyant":
+    text = "If you are feeling flamboyant, you should wear sequins!";
+    break;
+  case "sad":
+    text = "If you are feeling sad, you should never wear tracksuits - it just makes you more sad.";
+    break;
+  case "shy":
+    text = "If you are feeling shy, wear beige to blend into the background.";
+    break;
+  case "normal":
+    text = "Wear jeans and a T-shirt cos you ain't special.";
+    break;
+  case "hungry":
+    text = "Wear loose trousers, you're about to feast!";
+    break;
+  case "hungover":
+    text = "Wear a cone of shame.";
+    break;
+  case "drunk":
+    text = "Wear whatever you like, but wear something or you'll get arrested!";
+    break;
+  case "serious":
+    text = "If you are feeling serious, you're talking to the wrong chatbot.";
+    break;
+  case "casual":
+    text = "Don't think too much about what you're wearing. It's caj...";
+    break;
+  case "athletic":
+    text = "If you're considering exercise, perhaps reconsider your life choices.";
+    break;
+  case "grumpy":
+    text = "Wear a funny hat to cheer yourself up.";
+    break;
+  default:
+    text = "I have never heard of that mood...";
+  }
   
-  db.get("appointments").push({ date_time: date_time}).write()
-  res.status(200).json({fulfillmentMessages})
+  res.status(200).json({
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [text]
+        }
+      }
+    ]
+  })
 })
 
 module.exports = app;
